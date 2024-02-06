7f26b7233f5edc34cbcb5c8309f7409f083101e0
Georgy Felix <gerogyfelix@live.com>
2024-02-06 16:34:13 +0530

    Login fixed.

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,15 +1,24 @@
 import axios from "axios";
 import Cookies from "js-cookie";
-import API_ENDPOINT from '/global'
+import API_ENDPOINT from './global'
 
 const AUTH_API_URL =
 API_ENDPOINT + "/api/Authentication/Login";
 const VALIDATE_API_URL =
-API_ENDPOINT + "/api/Master/ValidateToken";
+API_ENDPOINT + "/api/Authentication/ValidateToken";
 
 export const login = async (username, password) => {
   try {
-    const response = await axios.post(AUTH_API_URL, { username, password });
+    const response = await axios.post(AUTH_API_URL, 
+      {
+        username: username,
+        password: password
+      },
+      {
+        headers: {
+          "serverId": "6c0d57e1-1e3b-46be-aa7f-4b95d97c853f"
+        }
+      });
     const token = response.data.token;
     Cookies.set("token", token, { expires: 1 / 24 });
     return token;
@@ -21,8 +30,13 @@ export const login = async (username, password) => {
 
 export const validateToken = async () => {
   try {
-    const token = Cookies.get("token");
-    const response = await axios.post(VALIDATE_API_URL, { token });
+    const token = "Bearer " + Cookies.get("token");
+    const response = await axios.post(VALIDATE_API_URL, {},
+      { 
+        headers: {
+          "Authorization": token
+        }
+      });
     return response.data.isValid;
   } catch (error) {
     console.error("Token validation failed:", error);
